2f52533edc1622c0605c1be629b95c7b6c02dc44
szikszail <sziklaszlo@gmail.com>
2021-03-12 20:24:49 +0100

    Added code

--- yamltopy/yamltopy.py
+++ yamltopy/yamltopy.py
@@ -5,10 +5,36 @@ yaml-to-py 0.0.1
 
 A CLI tool to convert YAML file to Python module for better load performance
 
-More info: http://github.com/szikszail/yaml-to-py
+More info: http://github.com/szikszail/yamltopy
 """
 
 import argparse
+import logging
+import yaml
+
+logger = logging.getLogger('yamltopy')
+
+
+def load_yaml(file):
+    logger.info('Loading YAML file: %s', file)
+    with open(file, 'r', encoding='utf-8') as f:
+        return yaml.load(f, yaml.FullLoader)
+
+
+def generate_py_content(name, data):
+    logger.info('Exported variable will be: %s', name)
+    content = []
+    if "datetime" in str(data):
+        content.append('import datetime')
+    content.append(f'{name} = {data}')
+    content.append('')
+    return '\n'.join(content)
+
+
+def save_py(file, content):
+    logger.info('Saving PY file: %s', file)
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write(content)
 
 
 def main():
@@ -20,9 +46,21 @@ def main():
     parser.add_argument('-i', '--input', default='input.yml',
                         help='The input Yaml file (YML or YAML)')
 
-    args = parser.parse_args()
+    parser.add_argument('-o', '--output', default='output.py',
+                        help='The output Py file')
 
-    print(f'Input: {args.input}')
+    parser.add_argument('-n', '--name', default='DATA',
+                        help='The name of the exported variable containg the Yaml data')
+
+    parser.add_argument('-v', '--verbose', default=False, action='store_true')
+
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+    data = load_yaml(args.input)
+    content = generate_py_content(args.name, data)
+    save_py(args.output, content)
+
 
 if __name__ == "__main__":
     main()
